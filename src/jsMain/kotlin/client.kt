b59ff5efd84cd4c316d738e85e1ef004d7ca6b98
Piotr Krzeminski <git@krzeminski.it>
2021-02-04 19:13:56 +0100

    Move React component to client.kt
    A single KT file for the frontend, for simplicity.
    
    Part of https://github.com/krzema12/kotlin-rpc/issues/24.

--- src/jsMain/kotlin/client.kt
+++ src/jsMain/kotlin/client.kt
@@ -1,9 +1,22 @@
+import it.krzeminski.todoapp.api.Todo
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.html.InputType
+import kotlinx.html.js.onClickFunction
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.div
+import react.dom.input
 import react.dom.render
 import react.router.dom.browserRouter
 import react.router.dom.route
 import react.router.dom.switch
+import react.setState
 
 fun main() {
     window.onload = {
@@ -11,8 +24,42 @@ fun main() {
             browserRouter {
                 switch {
                     route("/", exact = true) {
-                        child(Welcome::class) {}
+                        child(TodoApp::class) {}
                     }
+                }
+            }
+        }
+    }
+}
+
+external interface TodoAppState : RState {
+    var todos: List<Todo>
+}
+
+class TodoApp : RComponent<RProps, TodoAppState>(), CoroutineScope by MainScope() {
+
+    override fun RBuilder.render() {
+        div {
+            +"From backend: ${state.todos}"
+        }
+        input(type = InputType.button) {
+            attrs {
+                value = "Fetch TODOs"
+                onClickFunction = {
+                    launch {
+                        fetchTodos()
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun fetchTodos() {
+        with(TodoAppApiJsClient(url = "http://localhost:8080", coroutineContext)) {
+            launch {
+                val fetchedTodos = listTodos()
+                setState {
+                    todos = fetchedTodos
                 }
             }
         }
